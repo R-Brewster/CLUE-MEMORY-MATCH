11a2bd5e4538013865d68f0a8f999dda40b35db4
R-Brewster <rbrewster@ucmerced.edu>
2018-01-07 15:07:42 -0800

    Branch complete

--- controller.js
+++ controller.js
@@ -1,10 +1,20 @@
 function GameController () {
+    this.detectCrime = (roomIndex) => {
+        let matchedObjects = gameView.matchedObjects;
 
-    this.comparedObjects = () => {
-        let test = gameView.matchedObjects;
-        console.log(test)
+        let possibleCrime = matchedObjects[roomIndex];
+        console.log('possible crime', possibleCrime);
+
+        if(possibleCrime.item1 && possibleCrime.item2 !== undefined ) {
+           let crimeForThisRoom =  gameModel.crimes.find((crime) => {
+               return crime.room === possibleCrime.room
+           });
+            // console.log( 'crime for this room', crimeForThisRoom);
+
+            if(( (crimeForThisRoom.suspect ===possibleCrime.item1 || crimeForThisRoom.suspect ===possibleCrime.item2)) && ((crimeForThisRoom.weapon === possibleCrime.item1 || crimeForThisRoom.weapon === possibleCrime.item2))){
+                console.log('crime solved');
+            }
+        }
     };
-
-    this.comparedObjects();
 }
 
--- view.js
+++ view.js
@@ -42,53 +42,83 @@ function GameView() {
         if(ev.target.id !== undefined){
             ev.dataTransfer.setData("text", ev.target.id);
 
+            //If the parent element has two children, allow ondrop events again (this card is being removed from this room, so more cards should be allowed in)
             if(document.getElementById(ev.target.id).parentElement.childElementCount === 2) {
                 document.getElementById(ev.target.id).parentElement.setAttribute('ondrop', 'gameView.drop(event)')
             }
+
+            //If the card is being removed from a room, we'll need to know if it's an item1 or item2
+            let item1OrItem2 = '';
+
+            //If the card being dragged was already put in another room, find the index of that card in the matched objects array and record if that card is an item1 or item2
+            let cardIndexBeingRemovedFromRoom = this.matchedObjects.findIndex((possibleCrime) => {
+                if(possibleCrime.item1 === ev.target.id){
+                    item1OrItem2 = 'item1';
+                    return possibleCrime.item1
+                }
+                else if(possibleCrime.item2 === ev.target.id){
+                    item1OrItem2 = 'item2';
+                    return possibleCrime.item2
+                }
+
+            });
+
+            console.log('card being removed', cardIndexBeingRemovedFromRoom);
+
+            //If the card was an item1, remove the item1 from from the right object in this.matchedObjects, and if item2, do the same thing
+            switch(item1OrItem2){
+                case 'item1':
+                    this.matchedObjects[cardIndexBeingRemovedFromRoom].item1 = '';
+                    break;
+                case 'item2':
+                    this.matchedObjects[cardIndexBeingRemovedFromRoom].item2 = '';
+                    break;
+                default:
+                    return null;
+            }
+
+
         }
     };
 
     //Declares the data being moved and appends it to the element that it's being dropped on, then uses the information from the dropped element and its new parent to create a matchedObjects array for the model
     this.drop = (ev) =>{
-        // let matchedItems = {
-        //     suspect: '',
-        //     weapon: '',
-        //     room: '',
-        // };
         //Prevents error resulting from this being read before a drag and drop event has happened
         if(ev.dataTransfer !== undefined) {
             ev.preventDefault();
             let data = ev.dataTransfer.getData("text");
             ev.target.appendChild(document.getElementById(data));
 
-            console.log(document.getElementById(data));
-            console.log(document.getElementById(data).id);
-            console.log(document.getElementById(data).classList[1]);
+            // console.log(document.getElementById(data));
+            // console.log(document.getElementById(data).id);
+            // console.log(document.getElementById(data).classList[1]);
 
+            //Uses the room the card was dropped into (is now the parent element of the card) to find the index of the room in the crime list
             let parentRoomIndex = this.matchedObjects.findIndex((crime) => {
                 return crime.room === document.getElementById(data).parentElement.id
             });
 
+            //Item 1 of the correct room in the matchedObjects index is recorded as this card that was dropped
             if(this.matchedObjects[parentRoomIndex].item1 === ''){
                 this.matchedObjects[parentRoomIndex].item1 = document.getElementById(data).id
             }
 
+            //Item 2 of the correct room in the matchedObjects index is recorded as this card that was dropped
             else if(this.matchedObjects[parentRoomIndex].item2 === ''){
                 this.matchedObjects[parentRoomIndex].item2 = document.getElementById(data).id;
             }
 
+            //If the room has two items already matched, prevent any more cards being added by removing the ondrop attribute
             if(document.getElementById(data).parentElement.childElementCount === 2){
                 document.getElementById(data).parentElement.removeAttribute('ondrop')
             }
 
-            // if(document.getElementById(data).parentElement.childElementCount === 1){
-            //     document.getElementById(data).parentElement.setAttribute( 'ondrop', 'gameView.drop(event)')
-            // }
-
-            gameController.comparedObjects();
+            //After each card drop, check if that card matches a crime and if a crime is solved
+            gameController.detectCrime(parentRoomIndex);
         }
     };
 
+    //Used to record what cards have been dropped into which rooms
     this.matchedObjects = [
         {room: 'Ballroom', item1: '', item2: ''},
         {room: 'Dining Room', item1: '', item2: ''},
